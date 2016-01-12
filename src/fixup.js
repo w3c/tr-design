fc3a9c20e2fdd965863fd1d6b55b14117a932a87
fantasai <fantasai.bugs@inkedblade.net>
2016-01-12 12:50:46 -0800

    Attempt to fix some scrolling problems.

--- src/fixup.js
+++ src/fixup.js
@@ -5,10 +5,11 @@
  * It is intended to be a very simple script for 2016.                        *
  ******************************************************************************/
 
-function toggleSidebar(on) {
+function toggleSidebar(on, skipScroll) {
   if (on == undefined) {
     on = document.body.className != 'toc-sidebar'
   }
+
   toggle1 = document.getElementById('toc-toggle');
   toggle2 = document.getElementById('toc-toggle-inline');
   if (on) {
@@ -18,7 +19,9 @@ function toggleSidebar(on) {
     toggle1.title = "Collapse Sidebar";
     toggle2.innerHTML = "→";
     toggle2.title = "Collapse Sidebar";
-    window.scrollBy(0, 0 - tocHeight);
+    if (!skipScroll) {
+      window.scrollBy(0, 0 - tocHeight);
+    }
   }
   else {
     document.body.className = 'toc-inline';
@@ -27,37 +30,52 @@ function toggleSidebar(on) {
     toggle2.innerHTML = "←";
     toggle2.title = "Pop Out Sidebar";
     tocNav = document.getElementById('toc');
-    window.scrollBy(0, tocNav.offsetHeight);
+    if (!skipScroll) {
+      window.scrollBy(0, tocNav.offsetHeight);
+    }
   }
   return false;
 }
 
-toggle = document.createElement('a');
-  /* This should probably be a button, but appearance isn't standards-track.*/
-toggle.setAttribute('id', 'toc-toggle-inline');
-toggle.setAttribute('class', 'toc-toggle');
-toggle.setAttribute('href', '#toc');
-toggle.setAttribute('onclick', 'return toggleSidebar()');
-toggle.innerHTML = "←";
-
-tocNav = document.getElementById('toc');
-tocH = tocNav.getElementsByTagName('h2')[0];
-tocH.appendChild(document.createTextNode(' '));
-tocH.appendChild(toggle);
-
-toggle = toggle.cloneNode(true);
-toggle.setAttribute('id', 'toc-toggle');
-b2t = document.getElementById('back-to-top');
-if (b2t) {
-  b2t.appendChild(toggle);
-}
-else {
-  document.body.appendChild(toggle);
+function resizeListener() {
+  toggleSidebar(window.matchMedia('screen and (min-width: 78em)').matches, true);
 }
 
+function removeResizeToggle() {
+  toggle = document.getElementById('toc-toggle');
+  toggle.setAttribute('onclick', 'return toggleSidebar()');
+  toggle = document.getElementById('toc-toggle-inline');
+  toggle.setAttribute('onclick', 'return toggleSidebar()');
+  window.removeEventListener('resize', resizeListener, true);
+}
+
+function createSidebarToggle() {
+  toggle = document.createElement('a');
+    /* This should probably be a button, but appearance isn't standards-track.*/
+  toggle.setAttribute('id', 'toc-toggle-inline');
+  toggle.setAttribute('class', 'toc-toggle');
+  toggle.setAttribute('href', '#toc');
+  toggle.setAttribute('onclick', 'removeResizeToggle(); return toggleSidebar()');
+  toggle.innerHTML = "←";
+  
+  tocNav = document.getElementById('toc');
+  tocH = tocNav.getElementsByTagName('h2')[0];
+  tocH.appendChild(document.createTextNode(' '));
+  tocH.appendChild(toggle);
+  
+  toggle = toggle.cloneNode(true);
+  toggle.setAttribute('id', 'toc-toggle');
+  b2t = document.getElementById('back-to-top');
+  if (b2t) {
+    b2t.appendChild(toggle);
+  }
+  else {
+    document.body.appendChild(toggle);
+  }
+}
+
+createSidebarToggle();
 if (window.matchMedia) {
-  toggleSidebar(window.matchMedia('screen and (min-width: 78em)').matches);
-  window.addEventListener('resize', function() {
-    toggleSidebar(window.matchMedia('screen and (min-width: 78em)').matches);
-  }, true);
+  toggleSidebar(window.matchMedia('screen and (min-width: 78em)').matches, true);
+  window.addEventListener('resize', resizeListener);
 }
