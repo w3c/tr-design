9e3fd6ab5a9563140a06dd9b99e6514783bfdf7d
Tab Atkins <jackalmage@gmail.com>
2016-01-13 14:30:02 -0800

    Fixup sidebar JS

--- src/fixup.js
+++ src/fixup.js
@@ -4,78 +4,64 @@
  * This code handles some fixup to improve the table of contents.             *
  * It is intended to be a very simple script for 2016.                        *
  ******************************************************************************/
+(function(){
+  function toggleSidebar(on, skipScroll) {
+    if (on == undefined) {
+      on = !document.body.classList.contains('toc-sidebar');
+    }
 
-function toggleSidebar(on, skipScroll) {
-  if (on == undefined) {
-    on = document.body.className != 'toc-sidebar'
-  }
-
-  toggle1 = document.getElementById('toc-toggle');
-  toggle2 = document.getElementById('toc-toggle-inline');
-  if (on) {
-    tocHeight = tocNav.offsetHeight;
-    document.body.className = 'toc-sidebar';
-    toggle1.innerHTML = "←";
-    toggle1.title = "Collapse Sidebar";
-    toggle2.innerHTML = "→";
-    toggle2.title = "Collapse Sidebar";
-    if (!skipScroll) {
-      window.scrollBy(0, 0 - tocHeight);
+    var toggle = document.getElementById('toc-toggle');
+    var tocNav = document.getElementById('toc');
+    if (on) {
+      var tocHeight = tocNav.offsetHeight;
+      document.body.classList.add('toc-sidebar');
+      document.body.classList.remove('toc-inline');
+      toggle.textContent = "←";
+      toggle.title = "Collapse Sidebar";
+      if (!skipScroll) {
+        window.scrollBy(0, 0 - tocHeight);
+      }
+    }
+    else {
+      document.body.classList.add('toc-inline');
+      document.body.classList.remove('toc-sidebar');
+      toggle.textContent = "→";
+      toggle.title = "Pop Out Sidebar";
+      if (!skipScroll) {
+        window.scrollBy(0, tocNav.offsetHeight);
+      }
     }
   }
-  else {
-    document.body.className = 'toc-inline';
-    toggle1.innerHTML = "→";
-    toggle1.title = "Pop Out Sidebar";
-    toggle2.innerHTML = "←";
-    toggle2.title = "Pop Out Sidebar";
-    tocNav = document.getElementById('toc');
-    if (!skipScroll) {
-      window.scrollBy(0, tocNav.offsetHeight);
+
+  function createSidebarToggle() {
+    var toggle = document.createElement('a');
+      /* This should probably be a button, but appearance isn't standards-track.*/
+    toggle.setAttribute('id', 'toc-toggle');
+    toggle.setAttribute('class', 'toc-toggle');
+    toggle.setAttribute('href', '#toc');
+    toggle.addEventListener('click', function(e){ e.preventDefault(); toggleSidebar(); return false;}, false);
+    toggle.textContent = "←";
+
+    var b2t = document.getElementById('back-to-top');
+    if (b2t) {
+      b2t.appendChild(toggle);
+    }
+    else {
+      document.body.appendChild(toggle);
     }
   }
-  return false;
-}
 
-function resizeListener() {
-  toggleSidebar(window.matchMedia('screen and (min-width: 78em)').matches, true);
-}
+  createSidebarToggle();
+  var sidebarMedia = window.matchMedia('screen and (min-width: 78em)');
+  sidebarMedia.addEventListener('change', function(e){toggleSidebar(e.matches, true);}, false);
+  toggleSidebar(sidebarMedia.matches, true);
 
-function removeResizeToggle() {
-  toggle = document.getElementById('toc-toggle');
-  toggle.setAttribute('onclick', 'return toggleSidebar()');
-  toggle = document.getElementById('toc-toggle-inline');
-  toggle.setAttribute('onclick', 'return toggleSidebar()');
-  window.removeEventListener('resize', resizeListener);
-}
-
-function createSidebarToggle() {
-  toggle = document.createElement('a');
-    /* This should probably be a button, but appearance isn't standards-track.*/
-  toggle.setAttribute('id', 'toc-toggle-inline');
-  toggle.setAttribute('class', 'toc-toggle');
-  toggle.setAttribute('href', '#toc');
-  toggle.setAttribute('onclick', 'removeResizeToggle(); return toggleSidebar()');
-  toggle.innerHTML = "←";
-  
-  tocNav = document.getElementById('toc');
-  tocH = tocNav.getElementsByTagName('h2')[0];
-  tocH.appendChild(document.createTextNode(' '));
-  tocH.appendChild(toggle);
-  
-  toggle = toggle.cloneNode(true);
-  toggle.setAttribute('id', 'toc-toggle');
-  b2t = document.getElementById('back-to-top');
-  if (b2t) {
-    b2t.appendChild(toggle);
-  }
-  else {
-    document.body.appendChild(toggle);
-  }
-}
-
-createSidebarToggle();
-if (window.matchMedia) {
-  toggleSidebar(window.matchMedia('screen and (min-width: 78em)').matches, true);
-  window.addEventListener('resize', resizeListener);
-}
+  /* If the sidebar has been manually opened and is currently overlaying the text
+     (window too small for the MQ to add the margin to body),
+     then auto-close the sidebar once you click on something in there. */
+  document.getElementById('toc').addEventListener('click', function(e) {
+    if(e.target.tagName.toLowerCase() == "a" && document.body.classList.contains('toc-sidebar') && !sidebarMedia.matches) {
+      toggleSidebar();
+    }
+  }, false);
+})();
