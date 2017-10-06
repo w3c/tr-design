002c0f21f08c3573b43950ed7d007f6647a14924
Denis Ah-Kang <denis@w3.org>
2017-10-06 10:42:07 +0400

    deprecation warning code only for www.w3.org

--- src/fixup.js
+++ src/fixup.js
@@ -141,63 +141,63 @@
   }
 
   /* Deprecation warning */
-  var request = new XMLHttpRequest();
-
-  request.open('GET', '//www.w3.org/TR/tr-outdated-spec');
-  request.onload = function() {
-    if (request.status < 200 && request.status >= 400) {
-      return;
-    }
-    try {
-      var currentSpec = JSON.parse(request.responseText);
-    } catch (err) {
-      console.error(err);
-      return;
-    }
-    document.body.classList.add("outdated-spec");
-    var node = document.createElement("p");
-    node.classList.add("outdated-warning");
-
-    var frag = document.createDocumentFragment();
-    var heading = document.createElement("strong");
-    heading.innerHTML = currentSpec.header;
-    frag.appendChild(heading);
-
-    var anchor = document.createElement("a");
-    anchor.id = "outdated-note";
-    anchor.href = currentSpec.latestUrl;
-    anchor.innerText = currentSpec.latestUrl + ".";
-
-    var warning = document.createElement("span");
-    warning.innerText = currentSpec.warning;
-    warning.appendChild(anchor);
-    frag.appendChild(warning);
-
-    var button = document.createElement("button");
-    var handler = makeClickHandler(node);
-    button.addEventListener("click", handler);
-    button.innerHTML = "&#9662; collapse";
-    frag.appendChild(button);
-    node.appendChild(frag);
-
-    function makeClickHandler(node) {
-      var isOpen = true;
-      return function collapseWarning(event) {
-        var button = event.target;
-        isOpen = !isOpen;
-        node.classList.toggle("outdated-collapsed");
-        document.body.classList.toggle("outdated-spec");
-        button.innerText = (isOpen) ? '\u25BE collapse' : '\u25B4 expand';
-      }
-    }
-    document.body.appendChild(node);
-  };
-
-  request.onerror = function() {
-    console.error("Request to https://www.w3.org/TR/tr-outdated-spec failed.");
-  };
-
   if (document.location.hostname === "www.w3.org") {
+    var request = new XMLHttpRequest();
+
+    request.open('GET', '//www.w3.org/TR/tr-outdated-spec');
+    request.onload = function() {
+      if (request.status < 200 && request.status >= 400) {
+        return;
+      }
+      try {
+        var currentSpec = JSON.parse(request.responseText);
+      } catch (err) {
+        console.error(err);
+        return;
+      }
+      document.body.classList.add("outdated-spec");
+      var node = document.createElement("p");
+      node.classList.add("outdated-warning");
+
+      var frag = document.createDocumentFragment();
+      var heading = document.createElement("strong");
+      heading.innerHTML = currentSpec.header;
+      frag.appendChild(heading);
+
+      var anchor = document.createElement("a");
+      anchor.id = "outdated-note";
+      anchor.href = currentSpec.latestUrl;
+      anchor.innerText = currentSpec.latestUrl + ".";
+
+      var warning = document.createElement("span");
+      warning.innerText = currentSpec.warning;
+      warning.appendChild(anchor);
+      frag.appendChild(warning);
+
+      var button = document.createElement("button");
+      var handler = makeClickHandler(node);
+      button.addEventListener("click", handler);
+      button.innerHTML = "&#9662; collapse";
+      frag.appendChild(button);
+      node.appendChild(frag);
+
+      function makeClickHandler(node) {
+        var isOpen = true;
+        return function collapseWarning(event) {
+          var button = event.target;
+          isOpen = !isOpen;
+          node.classList.toggle("outdated-collapsed");
+          document.body.classList.toggle("outdated-spec");
+          button.innerText = (isOpen) ? '\u25BE collapse' : '\u25B4 expand';
+        }
+      }
+      document.body.appendChild(node);
+    };
+
+    request.onerror = function() {
+      console.error("Request to https://www.w3.org/TR/tr-outdated-spec failed.");
+    };
+
     request.send();
   }
 })();
