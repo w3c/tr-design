e40643f08e80f65a31d54ab2fb9e3ca34fa6cfcc
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2021-10-04 11:01:34 +0530

    fixup.js: exclude non-stylesheet link for darkCss (#257)

--- src/fixup.js
+++ src/fixup.js
@@ -298,7 +298,7 @@
   }
 
   /* Dark mode toggle */
-  const darkCss = document.querySelector('link[href^="https://www.w3.org/StyleSheets/TR/2016/dark"]');
+  const darkCss = document.querySelector('link[rel~="stylesheet"][href^="https://www.w3.org/StyleSheets/TR/2016/dark"]');
   if (darkCss) {
     const colorScheme = localStorage.getItem("tr-theme") || "auto";
     darkCss.disabled = colorScheme === "light";
